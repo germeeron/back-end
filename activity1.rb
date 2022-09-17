084b3f8d83e4d4347910c8b8c65d27ebb131cc84
germeeron <germeeron@gmail.com>
2022-09-17 12:26:36 +0800

    fourth commit

--- activity1.rb
+++ activity1.rb
@@ -1,3 +1,4 @@
-puts("hello world")
-puts("hello world")
-puts("hello world")
+contact_data = [["john@email.com", "123 Main st.", "555-123-4567"],
+            ["avion@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"John Cruz" => {}, "Avion School" => {}}
