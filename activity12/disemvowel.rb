f00793061e2b0b709bacb78ad98145cc45752c73
germeeron <germeeron@gmail.com>
2022-10-04 19:59:55 +0800

    activity12

--- activity12/disemvowel.rb
+++ activity12/disemvowel.rb
@@ -2,7 +2,7 @@ def disemvowel(string)
     new_arr = []
 
     string.each_char do |char|            # loop
-        unless char == "a" || char == "e" || char == "i" || char == "o" || char == "u" || char == "A" || char == "E" || char == "I" || char == "O" || char == "U"
+        if char == "a" || char == "e" || char == "i" || char == "o" || char == "u" || char == "A" || char == "E" || char == "I" || char == "O" || char == "U"
             new_arr << char
         end
     end
