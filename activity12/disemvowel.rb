c9d9965961c774b5712bcdbf1c1084a7514a428a
germeeron <germeeron@gmail.com>
2022-10-04 19:58:31 +0800

    activity12

--- activity12/disemvowel.rb
+++ activity12/disemvowel.rb
@@ -2,7 +2,7 @@ def disemvowel(string)
     new_arr = []
 
     string.each_char do |char|            # loop
-        if char != 'i'
+        unless char == "a" || char == "e" || char == "i" || char == "o" || char == "u" || char == "A" || char == "E" || char == "I" || char == "O" || char == "U"
             new_arr << char
         end
     end
--- activity12/disemvowel2.rb
+++
@@ -1,5 +0,0 @@
-def disemvowel(string)
-    string.delete("AEIOUaeiou")
-end
-
-puts disemvowel("This website is for losers LOL!")
